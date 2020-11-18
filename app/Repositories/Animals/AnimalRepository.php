f67a465c0ce33e17a57c4689d65af90635bb0437
Artem Shinkaruk <artasend@gmail.com>
2020-11-19 02:49:04 +0700

    Fix images

--- app/Repositories/Animals/AnimalRepository.php
+++ app/Repositories/Animals/AnimalRepository.php
@@ -7,9 +7,9 @@ class AnimalRepository
     public function getAnimalsForHomePage(): array
     {
         return [
-            (object) ['title' => 'Маня', 'image' => 'https://pet-uploads.adoptapet.com/2/6/4/294541558.jpg'],
+            (object) ['title' => 'Маня', 'image' => 'https://pet-uploads.adoptapet.com/5/8/7/352561196.jpg'],
             (object) ['title' => 'Дог', 'image' => 'https://pet-uploads.adoptapet.com/2/6/4/294541558.jpg'],
-            (object) ['title' => 'Альбус', 'image' => 'https://pet-uploads.adoptapet.com/2/6/4/294541558.jpg'],
+            (object) ['title' => 'Альбус', 'image' => 'https://pet-uploads.adoptapet.com/3/f/6/491945943.jpg'],
         ];
     }
 }
